06022e04b7da84eeaa875f384e8742fc42094386
G16C <gabrielcampello2002@gmail.com>
2023-02-05 20:42:13 -0300

    Testes atualizados

--- tests/test_isitkbs.py
+++ tests/test_isitkbs.py
@@ -1,6 +1,6 @@
 #Pytest
 import pytest
-from isitkbs import is_kbs
+from isitkbs import isitkbs
 
 @pytest.mark.parametrize("mashings, result1", [
     ("ldkajs", 1),
@@ -14,9 +14,9 @@ from isitkbs import is_kbs
     ("peqweqwe", 1),
 ])
 
-def test_mashings(mashings, result1):
-    assert is_kbs(input_data = mashings) == result1
-
+def test_wordm(mashings, result1):
+    w = isitkbs()
+    assert w.wordkbs(input_data = mashings) == result1
 
 @pytest.mark.parametrize("nonmashings, result2", [
     ("home", 0),
@@ -31,21 +31,21 @@ def test_mashings(mashings, result1):
     ("world", 0),
 ])
 
-def test_nonmashings(nonmashings, result2):
-    assert is_kbs(input_data = nonmashings) == result2
-
+def test_wordn(nonmashings, result2):
+    wn = isitkbs()
+    assert wn.wordkbs(input_data = nonmashings) == result2
 
 @pytest.mark.parametrize("phrases, result3", [
-    ("This is a asdjhasijd", [["asdjhasijd"]]),
-    ("a", 0),
-    ("b", 0),
-    ("The world is a beautiful place", 0),
-    ("Be careful ieieieie", [["ieieieie"]]),
-    ("It's time to dudududuel", [["dudududuel"]]),
-    ("This is just a akdshjsaj message aaaaaa", [["akdshjsaj"], ["aaaaaa"]]),
-    ("I love batataaskdhjaksj", [["batataaskdhjaksj"]])
+    ("This is a asdjhasijd", ["asdjhasijd"]),
+    ("a", []),
+    ("b", []),
+    ("The world is a beautiful place", []),
+    ("Be careful ieieieie", ["ieieieie"]),
+    ("It's time to dudududuel", ["dudududuel"]),
+    ("This is just a akdshjsaj message aaaaaa", ["akdshjsaj", "aaaaaa"]),
+    ("I love batataaskdhjaksj", ["batataaskdhjaksj"])
 ])
 
 def test_phrases(phrases, result3):
-    assert is_kbs(input_data = phrases, analyzer='phrases') == result3
-
+    ph = isitkbs()
+    assert ph.sentkbs(input_data = phrases) == result3
